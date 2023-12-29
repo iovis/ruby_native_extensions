6317bedd8f4189b317cd692a7fa2d2e385430226
David Marchante <davidmarchan@gmail.com>
2023-12-29 02:09:59 -0500

    Add back slow C implementation (sscanf)

--- c_day4/ext/c_day4/c_day4.c
+++ c_day4/ext/c_day4/c_day4.c
@@ -7,10 +7,12 @@ typedef struct Range {
 
 void range_print(Range range) { printf("%d..=%d\n", range.start, range.end); }
 
+void ranges_parse_slow(const char* line, Range ranges[static 2]) {
+  sscanf(line, "%d-%d,%d-%d", &ranges[0].start, &ranges[0].end,
+         &ranges[1].start, &ranges[1].end);
+}
+
 void ranges_parse(const char* line, Range ranges[static 2]) {
-  // NOTE: apparently `sscanf` is very slow
-  // sscanf(line, "%d-%d,%d-%d", &ranges[0].start, &ranges[0].end,
-  //        &ranges[1].start, &ranges[1].end);
   int i = 0;
   int digit = 0;
   int start = 0;
@@ -41,6 +43,42 @@ void ranges_parse(const char* line, Range ranges[static 2]) {
 bool range_full_overlap(Range self, Range other) {
   return (self.start <= other.start && self.end >= other.end) ||
          (other.start <= self.start && other.end >= self.end);
+}
+
+long p1_slow(size_t n, const char input[static n]) {
+  long count = 0;
+  Range ranges[2] = {0};
+
+  char line[32] = {0};
+  size_t li = 0;
+
+  for (size_t i = 0; i < n; i++) {
+    line[li++] = input[i];
+
+    if (input[i] == '\n') {
+      line[li] = '\0';
+      li = 0;
+
+      ranges_parse_slow(line, ranges);
+
+      if (range_full_overlap(ranges[0], ranges[1])) {
+        count++;
+      }
+    }
+  }
+
+  return count;
+}
+
+VALUE run_slow(VALUE self, VALUE input) {
+  Check_Type(input, T_STRING);
+
+  size_t length = RSTRING_LEN(input);
+  char* cstr_input = StringValuePtr(input);
+
+  long solution = p1_slow(length, cstr_input);
+
+  return LONG2FIX(solution);
 }
 
 long p1(size_t n, const char input[static n]) {
@@ -85,4 +123,5 @@ VALUE run(VALUE self, VALUE input) {
 RUBY_FUNC_EXPORTED void Init_c_day4(void) {
   VALUE rb_mCDay4 = rb_define_module("CDay4");
   rb_define_singleton_method(rb_mCDay4, "c_run", run, 1);
+  rb_define_singleton_method(rb_mCDay4, "c_run_slow", run_slow, 1);
 }
